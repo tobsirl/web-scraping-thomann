cb3ad9f16a20495120cc5df8d314dfe2f72d19a5
Paul Tobin <tobster111@gmail.com>
2019-03-29 11:17:51 +0000

    Data is now returned as an array of strings

--- scrapper.js
+++ scrapper.js
@@ -3,9 +3,16 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  const url = 'https://www.thomann.de/ie/st_models.html?pg=3&ls=25';
+  const url = 'https://www.thomann.de/ie/st_models.html?pg=2&ls=25';
   await page.goto(url);
-  await page.screenshot({ path: 'example.png' });
+  // await page.screenshot({ path: 'example.png' });
+
+  const textContent = await page.evaluate(() =>
+    Array.from(document.querySelectorAll('.title-block')).map(item =>
+      item.innerText.trim()
+    )
+  );
+  console.log(textContent);
 
   await browser.close();
 })();
