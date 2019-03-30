d59463d5d33d82e4c5baf12ee3465f5bfdd86fd2
Paul Tobin <tobster111@gmail.com>
2019-03-30 11:11:04 +0000

    Refactored to call a function named getGuitarInfo

--- scraper.js
+++ scraper.js
@@ -1,37 +1,44 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
+  const getGuitarInfo = async url => {
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    //title, price and image are scraped from the website and put in an object
+    const guitarInfo = await page.evaluate(() =>
+      Array.from(document.querySelectorAll('.extensible-article')).map(
+        content => ({
+          title: content.querySelector('.title-block').textContent,
+          price: content.querySelector('.price-block').textContent,
+          image: content.querySelector('.product-image img').src
+        })
+      )
+    );
+    return guitarInfo;
+  };
+
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+
   const url = 'https://www.thomann.de/ie/st_models.html?pg=2&ls=25';
-  await page.goto(url);
-  // await page.screenshot({ path: 'example.png' });
 
-  // const guitarNames = await page.evaluate(() =>
-  //   Array.from(document.querySelectorAll('.title-block')).map(guitar =>
-  //     guitar.innerText.trim()
-  //   )
-  // );
+  const guitarData = await getGuitarInfo(url);
 
-  // const guitarPrice = await page.evaluate(() =>
-  //   Array.from(document.querySelectorAll('.price-block')).map(price =>
-  //     price.innerText.trim()
-  //   )
-  // );
-  //title, price and image are scraped from the website and put in an object 
-  const guitarInfo = await page.evaluate(() =>
-    Array.from(document.querySelectorAll('.extensible-article')).map(
-      content => ({
-        title: content.querySelector('.title-block').textContent,
-        price: content.querySelector('.price-block').textContent,
-        image: content.querySelector('.product-image img').src
-      })
-    )
-  );
-
-  // console.log(guitarNames);
-  // console.log(guitarPrice);
-  console.log(guitarInfo);
+  console.log(guitarData);
 
   await browser.close();
 })();
+
+// await page.screenshot({ path: 'example.png' });
+
+// const guitarNames = await page.evaluate(() =>
+//   Array.from(document.querySelectorAll('.title-block')).map(guitar =>
+//     guitar.innerText.trim()
+//   )
+// );
+
+// const guitarPrice = await page.evaluate(() =>
+//   Array.from(document.querySelectorAll('.price-block')).map(price =>
+//     price.innerText.trim()
+//   )
+// );
