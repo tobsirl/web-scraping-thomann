bb31479607d43a96595840c70f7735c115cc9b10
Paul Tobin <tobster111@gmail.com>
2019-03-30 11:59:04 +0000

    Using regex to parse the url and change the page number

--- scraper.js
+++ scraper.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
   const getGuitarInfo = async url => {
     const page = await browser.newPage();
     await page.goto(url);
-
+    console.log(`Scraping page ${url}`);
     //title, price and image are scraped from the website and put in an object
     const guitarInfo = await page.evaluate(() =>
       Array.from(document.querySelectorAll('.extensible-article')).map(
@@ -15,12 +15,15 @@ const puppeteer = require('puppeteer');
         })
       )
     );
+    const nextPageNum = parseInt(url.match(/pg=(\d+)$/)[1], 10) + 1;
+    const nextURL = `https://www.thomann.de/ie/st_models.html?pg=${nextPageNum}`;
+    await page.close();
     return guitarInfo;
   };
 
   const browser = await puppeteer.launch();
 
-  const url = 'https://www.thomann.de/ie/st_models.html?pg=2&ls=25';
+  const url = 'https://www.thomann.de/ie/st_models.html?pg=1';
 
   const guitarData = await getGuitarInfo(url);
 
